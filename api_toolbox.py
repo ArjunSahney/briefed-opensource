afb6e2902af34e34c69cc8684d77b939bcc5fa23
ramgorthi04 <ramgorthi@gmail.com>
2024-02-18 20:22:53 -0500

    Finished re-building news summarization logic

---
+++ api_toolbox.py
@@ -0,0 +1,172 @@
+"""OpenAI API, News API, Newspaper3K library toolbox
+
+Functions:
+  Open AI API: 
+    * def get_gpt_response(prompt)
+
+  Serp API:
+    * def get_google_results(query)
+
+  Newspaper3K library:
+    * def get_article_text(url)
+
+  News API:
+    * def get_news_api_response(keywords)
+
+Written 2024-02-18
+
+Export API Keys, see 
+https://www.notion.so/API-Keys-66bf13bb561e42c08893108e2b0c8c02 for API keys
+"""
+
+import os
+from openai import OpenAI
+from newspaper import Article
+from newspaper.article import ArticleException
+import requests
+from serpapi import GoogleSearch
+
+__author__ = "Ram Gorthi, DJun Sahney"
+__version__ = "1.0"
+
+
+def get_gpt_response(prompt, gpt_model="gpt-4"):
+  """Encapsulates GPT prompting
+  User provides prompt and gets only the text of GPT response
+
+  Parameters
+  ----------
+  prompt : str
+  model : str, optional (default is "gpt-4")
+    Can also input "gpt-3.5-turbo"
+
+  Returns
+  -------
+  str
+    text response returned by chat completion agent
+  None
+    if no response received by GPT
+  """
+
+  client = OpenAI(
+      api_key=os.environ.get("openai_api_key"),
+  )
+  response = client.chat.completions.create(
+  messages=[
+    {
+      "role": "user",
+      "content": prompt,
+    }
+  ],
+  model=gpt_model,
+  )
+  if response.choices:
+    response_text = response.choices[0].message.content
+    return response_text
+  else:
+    return None
+
+def get_google_results(query, num_results, engine="google_news"):
+  """Returns JSON of articles fetched from google news
+  User provides query, gets 'news_results' JSON containing title, snippet, 
+  link, and source.
+
+  Parameters
+  ----------
+  query : str
+  num_results : int
+    Number of articles to return
+  engine : str, optional (default is "google_news")
+    Can also input "google" for google search engine
+
+  Returns
+  -------
+  JSON
+    news_results dictionary containing title, link, source, snippet for each
+    article
+  """
+  api_key=os.environ.get("serp_api_key")
+  params = {
+    "engine": engine,
+    "q": query,
+    "api_key": api_key,
+  }
+  search = GoogleSearch(params)
+  results = search.get_dict()
+  news_results = results["news_results"]
+  return news_results[:num_results]
+
+def get_article_text(url, part="body"):
+  """Returns requested part of article (body or title)
+  Uses newspaper 3k python library to download article, parse, and retrieve
+  text (without the bullshit on the page). Catches exceptions where download()
+  fails (typically paywalls).
+
+  Parameters
+  ----------
+  url : str
+  part : str (optional, default is "body" which returns body of article)
+    Supports "body" or "title"
+
+  Returns
+  -------
+  str
+    text or title of the article
+  """
+  article = Article(url)
+  try:
+    article.download()
+    article.parse()
+  except ArticleException as e:
+    print(f"Failed to download or parse article: {e}")
+    return None 
+  except Exception as e:
+    return None 
+  if (part == "body"):
+    return article.text
+  elif (part == "title"):
+    return article.title
+
+def get_news_api_response(query, get="articles", endpoint="/v2/everything"):
+  """Gets JSON of News API response to query
+  
+  Parameters
+  ----------
+  query : str
+    The search query string for the News API.
+  get : str, optional
+    The data from News API JSON to be retrieved, default is "articles"
+    Can also do "all"
+  endpoint : str, optional
+    The API endpoint to query. Default is "/v2/everything". Other valid options include:
+      * "/v2/top-headlines": For breaking news headlines for categories or countries.
+      * "/v2/top-headlines/sources": For breaking news headlines from the most notable sources.
+  
+  Returns
+  -------
+  List of dictionaries
+    Each dictionary represents an article from the News API response.
+  """
+  
+  url_stem = "https://newsapi.org"
+  url = url_stem + endpoint
+
+  api_key = os.environ.get("news_api_key")
+  params = {
+    "q": query, 
+    "apiKey": api_key
+  }
+
+  response = requests.get(url, params=params)
+  if (response.json().get('status') != "ok"):
+    print("Error: In get_news_api_response, failed to retreive News API response")
+    return None
+  
+  if (get == "all"):
+    return response.json()
+  if (get == "articles"):
+    related_articles = response.json().get('articles', [])
+    return related_articles
+  else:
+    print("Error: In get_news_api_response, invalid 'get'")
+    return None
--- summed.py
+++ summed.py
@@ -7,7 +7,6 @@ Written 2024-02-17
 Instructions: 
   1.Export API Keys, see https://www.notion.so/API-Keys-66bf13bb561e42c08893108e2b0c8c02
     for API keys
-  2.Call sum_by_keyword(keyword)
 """
 __author__ = "Ram Gorthi, DJun Sahney"
 __version__ = "1.0"
@@ -16,79 +15,12 @@ from newspaper import Article
 from newspaper.article import ArticleException
 from serpapi import GoogleSearch
 import os
+import requests
 from openai import OpenAI
+from api_toolbox import *
 
-def get_gpt_response(prompt, gpt_model="gpt-4"):
-  """Encapsulates GPT prompting
-  User provides prompt and gets only the text of GPT response
-
-  Parameters
-  ----------
-  prompt : str
-  model : str, optional (default is "gpt-4")
-    Can also input "gpt-3.5-turbo"
-
-  Returns
-  -------
-  str
-    text response returned by chat completion agent
-  None
-    if no response received by GPT
-  """
-
-  client = OpenAI(
-      api_key=os.environ.get("openai_api_key"),
-  )
-  response = client.chat.completions.create(
-  messages=[
-    {
-      "role": "user",
-      "content": prompt,
-    }
-  ],
-  model=gpt_model,
-  )
-  if response.choices:
-    response_text = response.choices[0].message.content
-    return response_text
-  else:
-    return None
-
-def get_google_results(query, num_results, engine="google_news"):
-  """Returns JSON of articles fetched from google news
-  User provides query, gets 'news_results' JSON containing title, snippet, 
-  link, and source.
-
-  Parameters
-  ----------
-  query : str
-  num_results : int
-    Number of articles to return
-  engine : str, optional (default is "google_news")
-    Can also input "google" for google search engine
-
-  Returns
-  -------
-  JSON
-    news_results dictionary containing title, link, source, snippet for each
-    article
-  """
-  api_key=os.environ.get("serp_api_key")
-  params = {
-    "engine": engine,
-    "q": query,
-    "api_key": api_key,
-  }
-  search = GoogleSearch(params)
-  results = search.get_dict()
-  news_results = results["news_results"]
-  return news_results[:num_results]
-
-def get_text(url):
-  """Returns text of article
-  Uses newspaper 3k python library to download article, parse, and retrieve
-  text (without the bullshit on the page). Catches exceptions where download()
-  fails (typically paywalls).
+def get_summary(url):
+  """Return short summary of article, using GPT-4
 
   Parameters
   ----------
@@ -97,21 +29,16 @@ def get_text(url):
   Returns
   -------
   str
-    text of the article
+    summary of article
   """
-  article = Article(url)
-  try:
-    article.download()
-    article.parse()
-  except ArticleException as e:
-    print(f"Failed to download or parse article: {e}")
-    return None 
-  except Exception as e:
-    return None 
-  return article.text
+  article_text = get_article_text(url)
+  if article_text is None:
+    return None
+  summary = get_gpt_response(f"Summarize the key points and essential information from the following article in a single paragraph of approximately 150 words. Focus on the most impactful facts and conclusions drawn in the article: {article_text}")
+  return summary
 
-def get_search_keywords(url):
-  """Gets 3-6 keywords based on the article text
+def get_search_keywords(url, method="summary"):
+  """Gets 3-6 keywords based on the article text using GPT
   TODO: Compare the keywords generated based on the article text vs article title, because
         retrieving the text takes some time but retrieving the title does not
         Down-side of using the article's text is that some articles with paywalls won't be
@@ -122,6 +49,9 @@ def get_search_keywords(url):
   Parameters
   ----------
   url : str
+  method : str (optional)
+    Generate keywords off of either summary or article title
+    Default is "summary," can also use "title"
 
   Returns
   -------
@@ -129,29 +59,86 @@ def get_search_keywords(url):
     3-6 keywords
   None if unable to get text of article
   """
-  article_text = get_text(url)
-  if article_text is None:
-    return None
-  keywords = get_gpt_response(f"Return a 3-6 word google search to retrieve more information about this article: {article_text}", "gpt-3.5-turbo")
+  if (method == "summary"):
+    article_text = get_article_text(url)
+    if article_text is None:
+      return None
+    keywords = get_gpt_response(f"Return a 3-6 word google search to retrieve more information about this topic: {article_text}", "gpt-3.5-turbo")
+  elif (method == "title"):
+    article_title = get_article_text(url, "title")
+    keywords = get_gpt_response(f"Optimize this article title into a 3-6 word google search query: {article_title}", "gpt-3.5-turbo")
   return keywords
 
-def get_combined_newsAPI_contents(keywords):
+def get_formatted_newsAPI_contents(keywords):
   """
-  TODO: Don't forget to include the SOURCES somehow
-        Will need to think some more about this algo and how to include only the sources
-        we actually use for the final summary. 
+  Get formatted contents of News API keyword search. Should return title, summary,
+  source, and URL in a clean format for purposes of inputting it into a summarizer
+  that will cite the correct sources. 
+
+  Strategy 1: Create a Dictionary where a number hashes to (article title, summary, source, URL)
+              Pass dictionary in to GPT and tell it to cite the article number. Then, we can
+              hyperlink the URL to the number and include source at bottom as a footnote.
+
+  Parameters
+  ----------
+  keywords : str
+
+  Returns
+  -------
+  articles_dict : {
+    1: {
+        "title": "Title 1"
+        "summary": "Summary 1"
+        "source": "Source 1"
+        "url": "https://example.com/article1"
+    }   
+    2: ...
+    ...  
+  }
   """
-  # TODO: bring in stuff from serp_custom 
 
+  # Strategy 1 (see docustring)
+  related_articles = get_news_api_response(keywords)
+  if (related_articles is None):
+    print("Error: In get_formatted_NewsAPI_contents(), received no articles")
+    return None
+  
+  articles_dict = {}
 
+  # Loop through the list of related articles, assigning each a unique number
+  for index, article in enumerate(related_articles, start=1):
+
+    title = article.get('title', 'No Title Available')
+    url = article.get('url', 'No URL Available')
+    date = article.get('publishedAt', 'No date Available')
+
+    # TODO: Build in a defensive check here for URL and within get_summary()
+    summary = get_summary(url)
+    source = article.get('source', {}).get('name', 'Unknown Source')
+
+    # Add the extracted information to the articles_dict, keyed by the index
+    articles_dict[index] = {
+      "title": title,
+      "summary": summary,
+      "source": source,
+      "date": date,
+      "url": url
+    }
+
+  return articles_dict
+    
 def sum_by_keyword(keyword):
   news_results = get_google_results(keyword, 10)
   for article in news_results:
     url = article.get('link')
     if url is not None:
-      keywords = get_search_keywords(url)
+      keywords = get_search_keywords(url, "summary")
       if keywords is not None:
-        prompt = f"Summarize: {get_combined_newsAPI_contents(keywords)}"
-        print(get_gpt_response(prompt))
+        formatted_contents = get_formatted_newsAPI_contents(keywords)
+        print(formatted_contents)
+        # prompt = f"Summarize the key points in this 'articles dictionary' into
+        #           a 150-word explainer. Cite the article number in parens when you
+        #           use information from a specific article. {formatted_contents}"
+        # print(get_gpt_response(prompt))
 
 sum_by_keyword("Trump")
