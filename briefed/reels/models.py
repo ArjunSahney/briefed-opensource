c29f870e0e8cbf164e55fa63b2a69f07509493d7
Arjun Code <arjuncode@Arjuns-MacBook-Air.local>
2024-05-21 15:38:26 -0400

    podcast initial

--- briefed/reels/models.py
+++ briefed/reels/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Reel(models.Model):
+    topic = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     summary = models.TextField()
     image_url = models.URLField()
